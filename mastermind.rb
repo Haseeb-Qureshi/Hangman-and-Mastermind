b04c34968ff2081564960c13cb9c8077940a9fbe
Haseeb-Qureshi <haseeb@haseebq.com>
2015-04-22 21:44:48 -0700

    Refactor Mastermind

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,54 @@
+class Game
+
+  def initialize
+    @correct_code = Code.new.randomize
+    @turns_passed = 0
+    @won = false
+  end
+
+  def play
+    until @won || @turns_passed == 8
+      input_code = Code.new.parse(get_input)
+      process(input_code)
+    end
+
+    puts @won ? "You did it!" : "Game over!"
+  end
+
+  def get_input
+    puts "Enter your guess, like so: \"RGBY\"."
+    gets.chomp[0..3]
+  end
+
+  def process(input_code)
+    if input_code.is_valid?
+      @turns_passed += 1
+      check(input_code)
+    else
+      render(error: true)
+    end
+  end
+
+  def check(input_code)
+    exact = input_code.exact_matches(@correct_code)
+    near = input_code.near_matches(@correct_code)
+    turns_remaining = 8 - @turns_passed
+
+    @won = true if exact == 4
+    render(exact: exact, near: near, turns: turns_remaining)
+  end
+
+  def render(params = {})
+    if params[:error]
+      puts "Not a valid guess!"
+    else
+      puts "You have #{params[:exact]} exact matches."
+      puts "You have #{params[:near]} correct colors."
+      puts "You have #{params[:turns]} turns left."
+    end
+  end
+end
+
 class Code
   attr_reader :pegs
   COLORS = %w[r g b y o p].map(&:to_sym)
@@ -36,60 +87,6 @@ class Code
   end
 
 end
-
-class Game
-
-  def initialize
-    @correct_code = Code.new.randomize
-    @turns_passed = 0
-    @won = false
-  end
-
-  def play
-    until game_over || @won
-      input_code = Code.new.parse(get_input)
-
-      if input_code.is_valid?
-        @turns_passed += 1
-        process(input_code)
-      else
-        render(error: true)
-      end
-    end
-
-    puts @won ? "You did it!" : "Game over!"
-  end
-
-  def process(input_code)
-    exact = input_code.exact_matches(@correct_code)
-    near = input_code.near_matches(@correct_code)
-    turns_remaining = 8 - @turns_passed
-
-    @won = true if exact == 4
-    render(exact: exact, near: near, turns: turns_remaining)
-  end
-
-  def game_over
-    @turns_passed >= 8
-  end
-
-  def get_input
-    puts "Enter your guess, like so: \"RGBY\"."
-    gets.chomp[0..3]
-  end
-
-  def render(params = {})
-    if params[:error]
-      puts "Not a valid guess!"
-    else
-      puts "You have #{params[:exact]} exact matches."
-      puts "You have #{params[:near]} correct colors."
-      puts "You have #{params[:turns]} turns left."
-    end
-  end
-end
-
-
 
 if __FILE__ == $PROGRAM_NAME
   Game.new.play
