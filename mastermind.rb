8ab5236b7774613eb449640be1560db46c60aa2a
Haseeb-Qureshi <haseeb@haseebq.com>
2015-04-22 18:49:30 -0700

    Refactor Mastermind

--- mastermind.rb
+++ mastermind.rb
@@ -1,14 +1,13 @@
 class Code
-
   attr_reader :pegs
-
   COLORS = %w[r g b y o p].map(&:to_sym)
 
   def initialize
     @pegs = []
   end
 
-  def random
+  def randomize
+    @pegs = []
     4.times { @pegs << COLORS.sample }
     self
   end
@@ -24,16 +23,14 @@ class Code
 
   def exact_matches(guess)
     count = 0
-    4.times { |i| @pegs[i] == guess.pegs[i] ? count += 1 : count }
+    4.times { |i| count += 1 if @pegs[i] == guess.pegs[i] }
     count
   end
 
   def near_matches(guess)
     count = 0
     4.times do |i|
-      if @pegs.include?(guess.pegs[i]) && @pegs[i] != guess.pegs[i]
-        count += 1
-      end
+      count += 1 if @pegs.include?(guess.pegs[i]) && @pegs[i] != guess.pegs[i]
     end
     count
   end
@@ -43,29 +40,32 @@ end
 class Game
 
   def initialize
-    @correct_code = Code.new.random
+    @correct_code = Code.new.randomize
     @turns_passed = 0
-    @got_it_right = false
+    @won = false
   end
 
   def play
-    until game_over || @got_it_right
-      input = Code.new.parse(get_input)
-      if input.is_valid?
+    until game_over || @won
+      input_code = Code.new.parse(get_input)
+
+      if input_code.is_valid?
         @turns_passed += 1
-        check(input)
+        process(input_code)
       else
         render(error: true)
       end
     end
-    puts @got_it_right ? "You did it!" : "Game over!"
+
+    puts @won ? "You did it!" : "Game over!"
   end
 
-  def check(input)
-    exact = input.exact_matches(@correct_code)
-    near = input.near_matches(@correct_code)
+  def process(input_code)
+    exact = input_code.exact_matches(@correct_code)
+    near = input_code.near_matches(@correct_code)
     turns_remaining = 8 - @turns_passed
-    @got_it_right = true if exact == 4
+
+    @won = true if exact == 4
     render(exact: exact, near: near, turns: turns_remaining)
   end
 
@@ -75,7 +75,7 @@ class Game
 
   def get_input
     puts "Enter your guess, like so: \"RGBY\"."
-    input = gets.chomp[0..3]
+    gets.chomp[0..3]
   end
 
   def render(params = {})
